c6c38464d6c9cc6881dbea2c59a29586e2c224e5
TashaGospel <nhanhnhanh17399@gmail.com>
2016-02-25 12:49:19 +0700

    Merge master: Login system

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
+var session = require('express-session');
 var path = require('path');
 var exec = require('child_process').exec;
+var bodyParser = require('body-parser');
+var renderPage = require('./bin/renderPage.js');
+var signup = require('./bin/signup.js');
+var login = require('./bin/login.js');
+var randString = require('./bin/randString.js');
+
+app.use(session({
+    secret: randString(10),
+    cookie: {maxAge: 3600000},
+    saveUninitialized: false,
+    resave: false
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 5000);
 
@@ -12,29 +27,27 @@ app.set('view engine', 'jade');
 // app.locals.pretty = true;
 
 app.get('/', function (request, response) {
-    exec('echo hello > last_request && ls', function(error, stdout, stderr) {
-        response.writeHead(200);
-        response.write('stdout: ' + stdout + '\n');
-        response.write('stderr: ' + stderr + '\n');
-        if (error !== null) {
-            response.write('exec error: ' + error);
-        }
-        response.end();
-    });
-    // response.redirect('/homepage.html');
+    response.redirect('/homepage.html');
 });
-
-function renderPage(response, file) {
-    fs.readFile('pages/' + file, function(error, data) {
-        if (!error) response.render('page', {content: data});
-        else renderPage(response, 'error.html');
-    });
-}
 
 app.get('/:file', function(request, response, next) {
     var file = request.params.file;
-    if (path.extname(file) === '.html') renderPage(response, file);
+    if (path.extname(file) === '.html') renderPage(response, file, request.session.username);
     else next();
+});
+
+app.post('/login', function(request, response) {
+   login(request, response, function(err) {
+       if (!err) response.redirect('/');
+       else response.redirect('/error.html');
+   })
+});
+
+app.post('/signup', function(request, response) {
+    signup(request, response, function(err) {
+        if (!err) response.redirect('/login.html');
+        else response.redirect('/error.html');
+    })
 });
 
 app.use(express.static('public'));
